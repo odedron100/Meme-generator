ff52a47fee0b93bda9b3f8438695859ecb99069b
odedron100 <odedron84@gmail.com>
2021-02-10 13:16:34 +0200

    Add render galery

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,12 +1,35 @@
 'use strict';
 
+const init = () => {
+  renderGallery();
+}
+
+const renderGallery = () => {
+  const imgs = getImgs();
+  const strHTMLS = imgs.map(img => {
+
+    return `
+      <img src="/img/${img.id}.jpg" onclick="onImgClicked(this)">
+    `
+  }).join(' ');
+  const elGallery = document.querySelector('.gallery-container');
+  elGallery.innerHTML = strHTMLS;
+}
+
 const onImgClicked = (img) => {
   openEditor();
 }
 
 const openEditor = () => {
-  const elGallery = document.querySelector('.main-content');
+  const elContent = document.querySelector('.main-content');
   const elEditor = document.querySelector('.meme-editor-container');
-  elGallery.style.display = 'none';
+  elContent.style.display = 'none';
   elEditor.style.display = 'grid';
+}
+
+const openOpenGallery = () => {
+  const elContent = document.querySelector('.main-content');
+  const elEditor = document.querySelector('.meme-editor-container');
+  elContent.style.display = 'grid';
+  elEditor.style.display = 'none';
 }
