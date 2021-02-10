71e0b8159f41192bf632161c0b37ab29c05b7c77
odedron100 <odedron84@gmail.com>
2021-02-10 12:57:16 +0200

    Add meme editor

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const onImgClicked = (img) => {
+  openEditor();
+}
+
+const openEditor = () => {
+  const elGallery = document.querySelector('.main-content');
+  const elEditor = document.querySelector('.meme-editor-container');
+  elGallery.style.display = 'none';
+  elEditor.style.display = 'grid';
+}
